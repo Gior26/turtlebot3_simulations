93ade98bab555e50f29fe6858390aedd852d69f3
Gior26 <37639274+Gior26@users.noreply.github.com>
2023-05-12 16:23:39 +0200

    Added files for 3 robots navigation

--- turtlebot3_gazebo/launch/turtlebot3_library.launch.py
+++ turtlebot3_gazebo/launch/turtlebot3_library.launch.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 #
-# Copyright 2019 ROBOTIS CO., LTD.
-#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -14,34 +12,38 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Authors: Joep Tool
+# Authors: Giordano Scarso
 
 import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
     launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
+    bringup_dir = get_package_share_directory('turtlebot3_gazebo')
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    x_pose1 = LaunchConfiguration('x_pose1', default='4.45')
-    y_pose1 = LaunchConfiguration('y_pose1', default='0.5')
-    x_pose2 = LaunchConfiguration('x_pose2', default='4.45')
-    y_pose2 = LaunchConfiguration('y_pose2', default='2.5')
-    x_pose3 = LaunchConfiguration('x_pose3', default='4.45')
-    y_pose3 = LaunchConfiguration('y_pose3', default='5.5')
+    world = LaunchConfiguration('world')
+    map_yaml_file = LaunchConfiguration('map')
 
-    world = os.path.join(
-        get_package_share_directory('turtlebot3_gazebo'),
-        'worlds',
-        'biblioteca.sdf'
-    )
+    declare_world_cmd = DeclareLaunchArgument(
+        'world',
+        default_value=os.path.join(bringup_dir, 'worlds', 'biblioteca.sdf'),
+        description='Full path to world file to load')
+
+    declare_map_yaml_cmd = DeclareLaunchArgument(
+        'map',
+        default_value=os.path.join(bringup_dir, 'maps', 'library-new.yaml'),
+        description='Full path to map file to load')
+
+    #world = os.path.join( get_package_share_directory('turtlebot3_gazebo'),
+    #    'worlds', 'biblioteca.sdf')
 
     gzserver_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -65,46 +67,35 @@ def generate_launch_description():
         ),
         launch_arguments={'use_sim_time': use_sim_time}.items()
     )
-    spawn_turtlebot_cmd1 = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(launch_file_dir, 'spawn_turtlebot3.launch.py')
-        ),
-        launch_arguments={
-            'x_pose': x_pose1,
-            'y_pose': y_pose1,
-            'namespace': 'tb3_1',
-        }.items()
-    )
 
-    spawn_turtlebot_cmd2 = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(launch_file_dir, 'spawn_turtlebot3_2.launch.py')
-        ),
-        launch_arguments={
-            'x_pose': x_pose2,
-            'y_pose': y_pose2,
-            'namespace': 'tb3_2',
-        }.items()
-    )
-    spawn_turtlebot_cmd3 = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(launch_file_dir, 'spawn_turtlebot3_3.launch.py')
-        ),
-        launch_arguments={
-            'x_pose': x_pose3,
-            'y_pose': y_pose3,
-            'namespace': 'tb3_3',
-        }.items()
-    )
+
+    multi_robot_cmd = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(launch_file_dir,
+                'multi_tb3_simulation_launch.py')),
+            launch_arguments={
+                'use_namespace': 'True',
+                'map': map_yaml_file,
+                'world': world,
+                'use_sim_time': 'True',
+                'autostart': 'True',
+                'use_rviz': 'False',
+                'use_simulator': 'False',
+                'use_composition': 'False',
+                'headless': 'True',
+                }.items())
+
+    timed_multirobot = TimerAction(period=5.0,
+            actions=[multi_robot_cmd])
+
 
     ld = LaunchDescription()
 
     # Add the commands to the launch description
+    ld.add_action(declare_world_cmd)
+    ld.add_action(declare_map_yaml_cmd)
     ld.add_action(gzserver_cmd)
     ld.add_action(gzclient_cmd)
     ld.add_action(robot_state_publisher_cmd)
-    ld.add_action(spawn_turtlebot_cmd1)
-    ld.add_action(spawn_turtlebot_cmd2)
-    ld.add_action(spawn_turtlebot_cmd3)
+    ld.add_action(timed_multirobot)
 
     return ld
